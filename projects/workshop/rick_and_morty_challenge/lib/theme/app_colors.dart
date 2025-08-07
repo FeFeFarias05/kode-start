6540202f2a98789eedbbe55e67924fcf65ee47c2
Fernanda Farias <fernanda.farias05@hotmail.com>
2025-08-07 01:19:04 -0300

    Colors, fonts and image inserted

---
+++ projects/workshop/rick_and_morty_challenge/lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color navigationBackground = const Color(0xff1C1B1F);
+  static Color profileIconColor = const Color(0xffCAC4D0);
+  static Color leftIconColor = const Color(0xffE6E1E5);
+  static Color cardDecriptionColor = const Color(0xff87A1FA);
+  static Color red = const Color(0xffD53C2E);
+  static Color green = const Color(0xff55CB44);
+  static Color backgroundColor = Colors.black;
+  static Color white = Colors.white;
+}
+
